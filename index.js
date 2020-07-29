93892a1258cd62e28950c7a4a78667ef1b6fadc3
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 06:01:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
