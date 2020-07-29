202ac4dae33dd9be69449e5b8fc420128d2bf8df
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-29 06:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  document.querySelector('#text').innerHTML = "This is really cool!"
 });
